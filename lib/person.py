71dd8d1768c77056f447581cff0a3f219360c0a7
philip dutra <monsterphilip10@gmail.com>
2023-07-24 14:01:31 -0400

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random 
 
 APPROVED_JOBS = [
     "Admin",
@@ -16,4 +17,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name ="Bob", job = random.choice(APPROVED_JOBS)):
+        self.name = name
+        self.job = job
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if (isinstance(name, str) and 1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+        
+    job = property(get_job, set_job)
